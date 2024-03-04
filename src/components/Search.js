bfb3e11dabd89056851a2f0693f1d3d47b4b0044
mryeminthu <dhammaarchive@hotmail.com>
2024-03-04 17:11:40 +0630

    Add 2fr for desktop view

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,24 +27,24 @@ const Search = () => {
 
   return (
     <div className='container'>
-      <input
-        type="text"
-        placeholder="Search for GitHub users..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+              <input
+          type="text"
+          placeholder="Search for GitHub users..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="input-full-width"
+        />
       <div>
-        {users.map((user) => (
-          <div key={user.id} className="user-card">
-            <img src={user.avatar_url} alt={`${user.login}'s avatar`} />
-            <h3>{user.login}</h3>
-            <p>Repositories: {user.public_repos}</p>
-            <a href={user.html_url} target="_blank" rel="noopener noreferrer">
-              Profile Link
-            </a>
-          </div>
-        ))}
-      </div>
+  {users.map((user) => (
+    <div key={user.id} className="user-card">
+      <img src={user.avatar_url} alt={`${user.login}'s avatar`} />
+      <h3>{user.login}</h3>
+      <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+        Profile Link
+      </a>
+    </div>
+  ))}
+</div>
     </div>
   );
 };
