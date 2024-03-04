e374a41744dab144557ca2260de781f994878084
mryeminthu <dhammaarchive@hotmail.com>
2024-03-04 16:05:35 +0630

    Add styles to app-title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Github User Search</h1>
+      <h1 className='app-title'>Github User Search</h1>
       <Search />
     </div>
   );
